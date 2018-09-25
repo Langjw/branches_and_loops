86c1c46df128c00bc7eb74baca3b76996909da7d
John Lang <langjw@s.dcsdk12.org>
2018-09-25 08:26:16 -0600

    add Excercise2

---
+++ Chapter24/Excercise2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Excercise2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter an integer for 'N': ");
+        int n = scan.nextInt();
+    }
+}
